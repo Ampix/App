39ab2f483539c731cfb683826f8181644a0298e1
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-15 23:00:32 +0100

    rpc teszt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-
+use discord_rpc_client::Client;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +8,20 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // Create the client
+    let mut drpc = Client::new(1147864570636030023);
+
+    // Start up the client connection, so that we can actually send and receive stuff
+    drpc.start();
+
+    // Set the activity
+    drpc.set_activity(|act| {
+        act.state("Indítja az Appot")
+            .details("Betöltés...")
+            .assets(|acs| acs.large_image("old_rounded").large_text("Ampix App"))
+    })
+    .expect("Hiba");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
