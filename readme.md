44d80b6947b9d2464b822b2eba19ae22ef346c78
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-06 21:00:42 +0200

    clear

--- readme.md
+++
@@ -1,8 +0,0 @@
-> [!NOTE]
-> Az Ampix App jelenleg munkálatok alatt áll.
-
-# Ampix App
-
-Az Ampix App semmire nem jó csak arra, hogy Rustot tanuljak.
-
-Ezen kívül le fogja váltani az [Ampix Updatert](https://github.com/Ampix/Updater) illetve egy kényelmes admin felülettel fog rendelkezni.
--- src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
